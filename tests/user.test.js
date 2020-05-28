3cfd9fe95d29c7e67bfdce803c5892970b7bcd50
Bruno Henrique Ferronato <bferronato@gmail.com>
2020-05-28 01:22:05 -0300

    Final test Section

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,24 +1,9 @@
 const request = require('supertest')
-const jwt = require('jsonwebtoken')
-const mongoose = require('mongoose')
 const app = require('../src/app')
 const User = require('../src/models/user')
+const { userOneId, userOne, setupDatabase } = require('./fixtures/db')
 
-const UserOneId = new mongoose.Types.ObjectId()
-const userOne = {
-    _id: UserOneId,
-    name: 'Bruno Henrique Ferronato',
-    email: 'bferronato@gmail.com',
-    password: 'teste123!',
-    tokens: [{
-        token: jwt.sign({ _id: UserOneId }, process.env.JWT_SECRET)
-    }]
-}
-
-beforeEach(async () => {
-    await User.deleteMany()
-    await new User(userOne).save()
-})
+beforeEach(setupDatabase)
 
 test('Should sigup a new user', async () => {
     const response = await request(app).post('/users').send({
@@ -102,7 +87,7 @@ test('Should upload avatar image', async () => {
         .attach('avatar', 'tests/fixtures/profile-pic.jpg')
         .expect(200)
 
-    const user = await User.findById(UserOneId)
+    const user = await User.findById(userOneId)
     expect(user.avatar).toEqual(expect.any(Buffer))
 })
 
